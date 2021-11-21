2eddd472e1df28dd9b687d1529b9d13255f13734
fukuda-hiroshi <74851971+fukuda-hiroshi@users.noreply.github.com>
2021-11-21 16:25:31 +0900

    first commit

---
+++ viewer.php
@@ -0,0 +1,24 @@
+<?php
+$player1 = $_POST['player1'];
+$player2 = $_POST['player2'];
+print 'player1='.$player1;
+print 'player2='.$player2;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>yonmoku｜yonmoku 3D</title>
+		<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+    <script src="three.min.js"></script>
+		<!-- <script src="/lib/OBJLoader.js"></script> -->
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script><!-- jQuery本体読み込み -->
+		<!-- <script type="module" src="viewer.js"></script> -->
+		<link rel="stylesheet" href="ui.css">
+	</head>
+  
+	<body>
+	</body>
+
+</html>
